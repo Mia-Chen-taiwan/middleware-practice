851c8172bb72f25e2ddf58cb3c09af7d3d8f606f
Mia Chen <mia89926@gmail.com>
2020-05-25 12:11:19 +0800

    feat: add middleware route

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use((req, res, next) => {
+  const time = new Date()
+  const request = `${req.method} from ${req.originalUrl}`
+  console.time(`${time.toLocaleString()} | ${request} | Costed time`)
+
+  res.on('finish', () => {
+    // 印出結果和傳輸時間
+    console.timeEnd(`${time.toLocaleString()} | ${request} | Costed time`)
+  })
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('列出全部 Todo')
-  console.log('Request Type:', req.method)
 })
 
 app.get('/new', (req, res) => {
